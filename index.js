7ac69365541c832c8d0da10b440aa86cd7605abf
MADOKA SUZUKI <158795547+MadokaSuzukim@users.noreply.github.com>
2024-03-23 15:02:41 +0900

    main

--- index.js
+++ index.js
@@ -17,31 +17,31 @@
 // }
 
 // アップロードが完了したら、動画のURLを含むメタデータをデータベースに保存
-function saveVideoData(videoUrl) {
-  const videosRef = databaseRef(db, 'videos');
-  const newVideoRef = push(videosRef);
-  set(newVideoRef, {
-    url: videoUrl,
-    uploadedAt: Date.now()
-  });
-}
+// function saveVideoData(videoUrl) {
+//   const videosRef = databaseRef(db, 'videos');
+//   const newVideoRef = push(videosRef);
+//   set(newVideoRef, {
+//     url: videoUrl,
+//     uploadedAt: Date.now()
+//   });
+// }
 
-// アップロード処理
-window.uploadVideo = async function() {
-    const fileInput = document.getElementById('videoFile');
-    if (fileInput.files.length === 0) {
-        console.log('No file selected.');
-        return;
-    }
-    const file = fileInput.files[0];
-    const fileRef = storageRef(storage, `videos/${file.name}`);
-    try {
-        const uploadResult = await uploadBytes(fileRef, file);
-        const url = await getDownloadURL(fileRef);
-        console.log('Upload successful, URL:', url);
-        displayUploadedVideo(url);
-        saveVideoData(url);
-    } catch (error) {
-        console.error('Upload failed:', error);
-    }
-};
+// // アップロード処理
+// window.uploadVideo = async function() {
+//     const fileInput = document.getElementById('videoFile');
+//     if (fileInput.files.length === 0) {
+//         console.log('No file selected.');
+//         return;
+//     }
+//     const file = fileInput.files[0];
+//     const fileRef = storageRef(storage, `videos/${file.name}`);
+//     try {
+//         const uploadResult = await uploadBytes(fileRef, file);
+//         const url = await getDownloadURL(fileRef);
+//         console.log('Upload successful, URL:', url);
+//         displayUploadedVideo(url);
+//         saveVideoData(url);
+//     } catch (error) {
+//         console.error('Upload failed:', error);
+//     }
+// };
