79720bdfab8247c92ae08037cb2ab294858a943a
MADOKA SUZUKI <158795547+MadokaSuzukim@users.noreply.github.com>
2024-03-23 14:58:36 +0900

    main

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// アップロードされた動画を表示する関数
-function displayUploadedVideo(url) {
-    // 要素を作成
-    const videoItem = document.createElement('div');
-    videoItem.classList.add('videoItem');
-    videoItem.innerHTML = `
-        <div class="dance-move" draggable="true">
-            <p>アップロードした動画</p>
-            <video width="320" height="240" controls>
-                <source src="${url}" type="video/mp4">
-                Your browser does not support the video tag.
-            </video>
-        </div>
-    `;
-    // 指定したコンテナ（idが'videosContainer'のdiv）に動画を追加
-    document.getElementById('videosContainer').appendChild(videoItem);
-}
+// // アップロードされた動画を表示する関数
+// function displayUploadedVideo(url) {
+//     // 要素を作成
+//     const videoItem = document.createElement('div');
+//     videoItem.classList.add('videoItem');
+//     videoItem.innerHTML = `
+//         <div class="dance-move" draggable="true">
+//             <p>アップロードした動画</p>
+//             <video width="320" height="240" controls>
+//                 <source src="${url}" type="video/mp4">
+//                 Your browser does not support the video tag.
+//             </video>
+//         </div>
+//     `;
+//     // 指定したコンテナ（idが'videosContainer'のdiv）に動画を追加
+//     document.getElementById('videosContainer').appendChild(videoItem);
+// }
 
 // アップロードが完了したら、動画のURLを含むメタデータをデータベースに保存
 function saveVideoData(videoUrl) {
